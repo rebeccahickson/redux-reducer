2549bf8f78693380a8362a939e964b5dc889ac12
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-05 11:49:01 -0500

    commit tree df344933c1ab7f5589c878fe643831d6ccdf8dd0

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  let newState;
+  switch (action.type) {
+    case "ADD_FRIEND":
+      newState = { friends: [...state.friends, action.friend] };
+      return newState;
+    case "REMOVE_FRIEND":
+      const indexToRemove = state.friends.findIndex(
+        (friend) => friend.id == action.id
+      );
+      newState = {
+        friends: [
+          ...state.friends.slice(0, indexToRemove),
+          ...state.friends.slice(indexToRemove + 1),
+        ],
+      };
+      return newState;
+    default:
+      return state;
+  }
 }
